cb1fad37874ba79ffdbb6d1f69163f264e1f66eb
KingArtisan <35980262+KingArtisan@users.noreply.github.com>
2018-02-09 16:31:25 -0700

    Create CreateGameResult.java

---
+++ Phase1_server/server/src/main/java/cs340/server/results/CreateGameResult.java
@@ -0,0 +1,21 @@
+package results;
+
+import java.util.ArrayList;
+
+import model.Game;
+
+public class CreateGameResult {
+	private ArrayList<Game> games;
+
+	public CreateGameResult(ArrayList<Game> games) {
+		this.games = games;
+	}
+
+	public ArrayList<Game> getGames() {
+		return games;
+	}
+
+	public void setGames(ArrayList<Game> games) {
+		this.games = games;
+	}
+}
