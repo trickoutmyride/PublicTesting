9a73bef7ec5c5d0147de23f889b8f972cc0cffbf
KingArtisan <35980262+KingArtisan@users.noreply.github.com>
2018-02-09 16:22:40 -0700

    Add files via upload

---
+++ Phase1_server/server/src/main/java/cs340/server/requests/CreateGameRequest.java
@@ -0,0 +1,49 @@
+package requests;
+
+import model.Player;
+
+public class CreateGameRequest {
+	private String name;
+	private Player player;
+	private int capacity;
+	private String color;
+	
+	public CreateGameRequest(String name, Player player, int capacity, String color) {
+		this.name = name;
+		this.player = player;
+		this.capacity = capacity;
+		this.color = color;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Player getPlayer() {
+		return player;
+	}
+
+	public void setPlayer(Player player) {
+		this.player = player;
+	}
+
+	public int getCapacity() {
+		return capacity;
+	}
+
+	public void setCapacity(int capacity) {
+		this.capacity = capacity;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+}
