87f7a9780e26c0f700bb0d2fee1a91eb1e48c964
markwelker <35548890+markwelker@users.noreply.github.com>
2018-02-12 16:13:47 -0700

    Update ClientFacade.java

--- Phase1_client/shared/src/main/java/cs340/shared/services/ClientFacade.java
+++ Phase1_client/shared/src/main/java/cs340/shared/services/ClientFacade.java
@@ -7,27 +7,31 @@ import cs340.shared.interfaces.IClient;
  * I need things to fill this with.
  */
 public class ClientFacade implements IClient {
-	public void createGame(Object request) {
-
+	public void createGame(Object game) {
+		CreateGameService.onCreateGame((Game) game);
 	}
 
-	public void joinGame(Object request) {
-
+	public void joinGame(Object game) {
+		JoinGameService.onJoinGame((Game) game);
 	}
 
-	public void login(Object request) {
-
+	public void login(Object player) {
+		LoginService.onLogin((Player) player);
 	}
 
-	public void register(Object request) {
-
+	public void register(Object player) {
+		RegisterService.onRegister((Player) player);
 	}
 
-	public void startGame(Object request) {
-
+	public void startGame() {
+		StartGameService.onStartGame();
+	}
+	
+	public void updateGameList(Object gameList) {
+		UpdateGameListService.onUpdateGameList((ArrayList<Game>) gameList);
 	}
 
-	public void error(Object request) {
-
+	public void error(Object error) {
+		ErrorService.onError((String) error);
 	}
 }
